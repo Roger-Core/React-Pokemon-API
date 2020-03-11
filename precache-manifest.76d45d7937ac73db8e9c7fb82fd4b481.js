e0052bbfe39e9b2b82a4ea2bf78871d12940eb35
Roger <thrasher_royerr666@hotmail.com>
2020-03-11 15:38:00 -0600

    Updates

---
+++ precache-manifest.76d45d7937ac73db8e9c7fb82fd4b481.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dfbf03198b5ef094f6ab6edea7ea10d",
+    "url": "/React-Pokemon-API/index.html"
+  },
+  {
+    "revision": "e10382e61a292954ddef",
+    "url": "/React-Pokemon-API/static/css/main.95dec386.chunk.css"
+  },
+  {
+    "revision": "a6518e4397891cf60bbc",
+    "url": "/React-Pokemon-API/static/js/2.abc4a146.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/React-Pokemon-API/static/js/2.abc4a146.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e10382e61a292954ddef",
+    "url": "/React-Pokemon-API/static/js/main.2a18e70d.chunk.js"
+  },
+  {
+    "revision": "2bf8b5091b2af42c6c8d",
+    "url": "/React-Pokemon-API/static/js/runtime-main.8f8795a6.js"
+  }
+]);
